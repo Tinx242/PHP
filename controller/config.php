385bc1241ec4d8f4a8ddf029c9ba0377c7be2aae
MARTHIN <likouangaclotairemarthin@gmail.com>
2025-03-24 16:55:56 +0100

    php date

--- controller/config.php
+++ controller/config.php
@@ -1,4 +1,11 @@
 <?php
-    const title = "Started PHP";
+    
 	$lang = ["fr","en","it"];
 	const css = "./css/style.css";
+    $version = phpversion();
+    const title = "Started PHP version";
+    $date = new DateTime(); # instance de class DateTime
+
+    ?>
+
+
--- index.php
+++ index.php
@@ -9,19 +9,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php print title ?></title>
+    <title><?php print title .$version ?></title>
 	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
     <link rel="manifest" href="favicon/site.webmanifest">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="<?= css ?>">
 </head>
 <body>
 	<!--cours https://hackmd.io/iNktz6XzRyu7gGmKvPpXfQ?view-->
 	<header>
 		<h1>
 			🚀 
-			<?php print title ?>
+			<?= title." ".$version ?>
 		</h1>
 	</header>
 	<main>
@@ -29,7 +29,7 @@
             <img src="./asset/cover.svg" alt="cover php">
             <figcaption>
                 <h2>
-                    PHP version 8
+                    PHP version<?= $version ?>
                 </h2>
             </figcaption>
         </figure>
@@ -47,7 +47,9 @@
 	<!-- <pre>
 	</pre> -->
 	<footer>
-		<p>&copy; - MIT - 2021</p>
+		<p>&copy; - MIT - <time datetime="<?= $date->format('Y-m-d') ?>"> 2021</time>
+		<?= $date->format('Y') ?>
+	</p>
 	</footer>
 	
 </html>
